8e31ed71eb70160e2600f3c15c50bc674620ae67
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-05-17 16:43:26 +0200

    Printf project header file

--- main.h
+++ main.h
@@ -1,5 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int fun_string(va_list arguments);
