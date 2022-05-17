57be42a776cd09ef7fd42a1a999c6d1adb6b979e
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-05-17 15:58:57 +0200

    Printf project header file

--- main.h
+++ main.h
@@ -1,36 +1,8 @@
-#ifndef header_file
-#define header_file
-
-#include <unistd.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-/**
- * struct typeprint - create libraries
- * @typec: char
- * @fun: point void of lista
- * Description: struct
- */
-typedef struct typeprint
-{
-	char typec;
-	int (*fun)(va_list lista);
-} typedate;
-/*Second function for _printf */
-int opFunction(int cont, va_list list, typedate tipos[],
-const char *format);
-int printUnk(int i, int spaces, const char *format, int band);
-int _strcmp(char *s1, char *s2);
-int _putchar(char c);
+#ifndef MAIN_H
+#ifdef MAIN_H
 int _printf(const char *format, ...);
-int printString(va_list lista);
-int printChar(va_list lista);
-int printDecimal(va_list lista);
-int printInteger(va_list lista);
-int printBinary(va_list lista);
-int printUnsigned(va_list lista);
-int printUnsignedDigit(unsigned int num, int *cont);
-int printOctal(va_list lista);
-int printDigitOctal(unsigned int num, int *cont);
-char *_littleBuffer(int size);
-#endif /* Libraries Printf Giovanni and Lina */
+int _putchar(char c);
+int fun_string(va_list arguments);
+int fun_character(va_list arguments);
+int fun_integer(va_list arguments);
+#endif
